4926a2358c3669920ffb3c679ac796346dc09806
Rory <rorygran5@gmail.com>
2022-04-27 12:24:29 -0400

    commit tree 9f8cd4123c147fd05da1ba7e09cad8acccec6f48

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function findWin(element){
+    return element.result === 'W';
+}
+
+function superbowlWin(arr){
+   let f = arr.find(findWin);
+   if (f){
+       return f.year;
+
+   }else{
+       return undefined;
+   }
+
+}
+
